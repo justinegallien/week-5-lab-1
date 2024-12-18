6ffb8774e05822842e0edd25ffcff3b3f8aa0350
Justine Gallien <justinegallien@gmail.com>
2024-12-18 11:50:27 -0600

    First Commit

---
+++ java.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let firstName = "Justine";
+let lastName = "Gallien";
+let email = firstName + "." + lastName + "@gmail.com";
+
+document.write(email);
---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+let C = prompt("What is the current temperature in Celsius?");
+
+let F = (C * 9) / 5 + 32;
+
+document.write("The current temperature is + 'F' + Farenheit.");
